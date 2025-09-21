2c12b4b2c30cd78cf9656f956c69932fa583cac3
sknam <glaranam@gmail.com>
2025-09-21 17:17:42 +0900

    test

--- KAIST_25fall/advanced financial quant/NeoMatrixAI/futures/mean_reversion/mean_reversion_config.py
+++ KAIST_25fall/advanced financial quant/NeoMatrixAI/futures/mean_reversion/mean_reversion_config.py
@@ -3,8 +3,8 @@ strategy_config = {
     "num_std": 2,
     "long_allocation_pct": 0.5,
     "short_allocation_pct": 0.5,
-    "strength_power": 1.0,
-    "strength_cap": 3.0,
+    "strength_power": 1.0, # (z_score - std)^strength power 
+    "strength_cap": 3.0, # z_score와 std 값의 cap을 둠 , min(z - std , cap)
     "fallback_enabled": True,
     "fallback_top_n": 2,
     "fallback_frac_cap": 0.5,
